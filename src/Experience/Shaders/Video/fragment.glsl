248215a0a7a8af221e49e1396c9bff793809d3e1
StevieCode <sdcowan@hotmail.com>
2022-07-02 01:20:23 +0100

    better shader

--- src/Experience/Shaders/Video/fragment.glsl
+++ src/Experience/Shaders/Video/fragment.glsl
@@ -21,25 +21,28 @@ varying vec2 vUv;
 
 void main()
 {
+    // 0 left 1 right
     float alpha = (clamp(vUv.x, 0.02, 1.0) - 0.02) * 0.999;
+    // 1 left 0 right
     float alpha2 = (1.0 - clamp(vUv.x, 0.02, 1.0) - 0.02) * 0.999;
+    // 0 bottom 1 top
     float alpha3 = (clamp(vUv.y, 0.02, 1.0) - 0.02) * 0.999;
+    // 1 top 0 bottom
     float alpha4 = (1.0 - clamp(vUv.y, 0.02, 1.0) - 0.02) * 0.999;
     
     // // result                    //short               //long
     float resultAlpha = min( min(alpha, alpha2), min(alpha3, alpha4));
 
-    // float strength = clamp(0.5, 0.7, resultAlpha)
+    // vec2 directionVector = vec2(0.5, 0.5) - vUv;
+    // float magPreSquare = (directionVector.x * directionVector.x) + (directionVector.y * directionVector.y);
+    // float mag = magPreSquare / magPreSquare;
+    // float magFlipped = 1.0 - mag;
 
-    // // vec3 col = color;
+    float distance = distance(vec2(0.5, 0.5) ,vUv);
 
+    float distanceFlipped = 1.0 - distance;
 
-    // vec3 colorResult = mix(uColorEnd, uColorStart, (vUv.y + sin(uTime)));
-
-    // vec3 col = colorResult;
-
-    // gl_FragColor=vec4(col, resultAlpha * opacity);
     vec4 t = texture2D(uTexture, vUv);
-    t.a = resultAlpha ;
+    t.a = distanceFlipped - 0.5;
     gl_FragColor = t;
 }
